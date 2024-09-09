a22ee9e1246d1c2d8362e43db97dcfcfabcd2f8d
agiletom <agiletom.coding.monster@gmail.com>
2024-09-09 20:17:13 +0300

    update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,33 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config'; // Import ConfigModule and ConfigService
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+
+import { VehicleModule } from './modules/vehicle/vehicle.module';
+import { ParseXmlService } from './common/services/parse-xml.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }), // Load environment variables globally
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('DATABASE_URL'), // Use DATABASE_URL from .env
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }),
+      inject: [ConfigService],
+    }),
+    VehicleModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'), // Auto-generate schema
+      playground: true, // Enable GraphQL Playground
+      introspection: true, // Enable schema introspection
+    }),
+  ],
+  providers: [ParseXmlService],
 })
 export class AppModule {}
