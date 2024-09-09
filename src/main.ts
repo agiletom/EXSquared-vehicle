719b24669c13b924c1f70043f3dd8be42d6531fb
agiletom <agiletom.coding.monster@gmail.com>
2024-09-09 20:19:39 +0300

    update main.js to ut issue

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { Logger } from '@nestjs/common';
+
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+export async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+  );
+  app.enableCors();
   await app.listen(3000);
+  Logger.log('Application is running on http://localhost:3000');
 }
 bootstrap();
